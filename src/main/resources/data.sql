ba0609fb9f52b28fca7dab02ba37d33f34d05a2b
stanislavma <msvdev@yandex.com>
2024-06-18 13:30:07 +0500

    fix: change data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,10 @@
+DELETE FROM requests ;
 DELETE FROM comments ;
 DELETE FROM bookings ;
 DELETE FROM items ;
 DELETE FROM users ;
 
+ALTER SEQUENCE requests_id_seq RESTART WITH 1;
 ALTER SEQUENCE comments_id_seq RESTART WITH 1;
 ALTER SEQUENCE bookings_id_seq RESTART WITH 1;
 ALTER SEQUENCE items_id_seq RESTART WITH 1;
